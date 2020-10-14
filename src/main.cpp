01ac7255a911fe44c982fa93049a8d2adec8e9d6
ben <nofe4rz@gmail.com>
2020-10-14 19:05:24 +0200

    added sensors

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,23 @@
 #include <ArduinoUniqueID.h>
 #include <VoltageReference.h>
 #include <Adafruit_Si7021.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <Wire.h>
+#include <Adafruit_BMP280.h>
 
-#define DEBUG
+#define INFO 
+//#define DEBUG
 
 // sensors
-#define SENSOR_TYPE     "si7021"
+#define SENSOR_TYPE_1   "si7021"
+#define SENSOR_TYPE_2   "ds18b20"
+#define SENSOR_TYPE_3   "bmp280"
 #define SENSOR_PIN_1    0 // sda
 #define SENSOR_PIN_2    2 // sdc
-#define DS_LONG         2 // deepsleep min
+#define SENSOR_PIN_3    3 // onewire
+#define DS_L           10 // deepsleep min
+#define DS_S            2 // deepsleep min
 
 // CC1101
 CC1101 cc = new Module(10, 2, RADIOLIB_NC);
@@ -22,6 +31,11 @@ VoltageReference vRef;
 
 // Si7021
 Adafruit_Si7021 si = Adafruit_Si7021();
+// DS18B20
+OneWire oneWire(SENSOR_PIN_3);
+DallasTemperature ds18b20(&oneWire);
+// BMP280
+Adafruit_BMP280 bmp280;
 
 // counter
 uint16_t msgCounter = 1;
@@ -33,7 +47,7 @@ void printHex(uint8_t num);
 void setup() {
   Serial.begin(9600);
   delay(10);
-#ifdef DEBUG
+#ifdef INFO
   delay(20);
 #endif
   // Start Boot
@@ -49,7 +63,7 @@ void setup() {
   } else {
     Serial.print(F("ERR "));
     Serial.println(state);
-    sleepDeep(1);
+    sleepDeep(DS_S);
   }
   // voltage
   vRef.begin();
@@ -57,20 +71,45 @@ void setup() {
   si.begin();
   /*if (!si.begin()){
     Serial.println("Did not find Si7021 sensor!");
-    sleepDeep(1);
+    sleepDeep(DS_S);
   }*/
+  // DS18B20
+  ds18b20.begin();
+  // BMP280
+  bmp280.begin(0x76,0x60); // fix GY-B11 module
 }
 
 void loop() {
-  float temperature = si.readTemperature(); 
-  float humidity = si.readHumidity();
-  if (!isnan(temperature)) {
-    Serial.print(SENSOR_TYPE);
+  float si_temperature = si.readTemperature(); 
+  float si_humidity = si.readHumidity();
+  if (!isnan(si_temperature)) {
+    Serial.print(SENSOR_TYPE_1);
     Serial.print(": ");
-    Serial.print(temperature);
+    Serial.print(si_temperature);
     Serial.print("C, ");
-    Serial.print(humidity);
+    Serial.print(si_humidity);
     Serial.println("%, ");
+  }
+  ds18b20.requestTemperatures();
+  float ds_temperature = ds18b20.getTempCByIndex(0);
+  if (ds_temperature != DEVICE_DISCONNECTED_C) {
+    Serial.print(SENSOR_TYPE_2);
+    Serial.print(": ");
+    Serial.print(ds_temperature);
+    Serial.println("C");
+  }
+  float bmp280_temperature = bmp280.readTemperature();
+  float bmp280_pressure = bmp280.readPressure();
+  float bmp280_altitude = bmp280.readAltitude(1013.25);
+  if (!isnan(bmp280_pressure) || bmp280_pressure > 0) {
+    Serial.print(SENSOR_TYPE_3);
+    Serial.print(": ");
+    Serial.print(bmp280_temperature);
+    Serial.print("C, ");
+    Serial.print(bmp280_pressure);
+    Serial.print("Pa, ");
+    Serial.print(bmp280_altitude);
+    Serial.println("m");
   }
 
   float vcc = vRef.readVcc()/100;
@@ -78,34 +117,54 @@ void loop() {
   Serial.print(vcc);
 
   // prepare msg string
-  //long randNum = random(0,9);
-  String str = "M,I:";
+  String str = "M";
+#ifdef DEBUG
+  str += ",I:";
   str += msgCounter;
-  //str += randNum;
+#endif
   str += ",N:";
   str += getUniqueID();
-  if (!isnan(temperature)) {
+  if (!isnan(si_temperature)) {
     str += ",T1:";
-    str += int(round(temperature*10));
+    str += int(round(si_temperature*10));
     str += ",H1:";
-    str += int(round(humidity*10));
+    str += int(round(si_humidity*10));
+  }
+  if (ds_temperature != DEVICE_DISCONNECTED_C) {
+    str += ",T2:";
+    str += int(round(ds_temperature*10));
+  }
+  if (!isnan(bmp280_pressure) || bmp280_pressure > 0) {
+    str += ",T3:";
+    str += int(round(bmp280_temperature*10));
+    str += ",P1:";
+    str += int(round(bmp280_pressure)/10);
+    str += ",A1:";
+    str += int(round(bmp280_altitude));
   }
   str += ",V1:";
   str += int(vcc);
-  str += ",E:";
 
   if (str.length() > 60){
-    Serial.println(F("> String too long"));
-    sleepDeep(1);
-  }
+    Serial.print(F("> String too long: "));
+    Serial.println(str.length());
+    sleepDeep(DS_S);
+  } else {
+    Serial.print(F("> String length: "));
+    Serial.println(str.length());
+    //str += ",E:";
+    int str_diff = 60 - str.length();
+    if (str_diff >= 1){ str += ",";}
+    if (str_diff >= 2){ str += "E";}
+    if (str_diff >= 3){ str += ":";}
 
-  // max length is 62 because of Arduino String last byte 00
-  // but 62 not good better use 61
-  // String length here to 60, thus packet length 61
-  for (uint8_t i = str.length(); i < 60; i++){
-    str += "0";
+    // max length is 62 because of Arduino String last byte 00
+    // but 62 not good better use 61
+    // String length here to 60, thus packet length 61
+    for (uint8_t i = str.length(); i < 60; i++){
+      str += "0";
+    }
   }
-
   Serial.print(F("[CC1101] Transmitting packet... "));
   // String to byte +1 string nul terminator 00 and overwrite 
   byte byteArr[str.length()+1];
@@ -130,7 +189,7 @@ void loop() {
     Serial.print(F("ERR, code "));
     Serial.println(state);
   }
-  sleepDeep(DS_LONG);
+  sleepDeep(DS_L);
   msgCounter++;
 }
 
@@ -173,7 +232,7 @@ String getUniqueID(){
 // 0 = forever
 void sleepDeep(uint8_t t) {
   uint8_t m = 60;
-#ifdef DEBUG
+#ifdef INFO
   Serial.print("Deep Sleep: ");
   if (t < 1){
     Serial.println("forever");
