11d412d08afa64403f65835f188fd3faefea5aff
ben <nofe4rz@gmail.com>
2023-01-07 23:06:07 +0100

    refactoring

--- include/config-sample.h
+++ include/config-sample.h
@@ -1,22 +1,23 @@
 // Disable/enable sensors
-#define SENSOR_TYPE_si7021    "si7021"
-#define SENSOR_TYPE_ds18b20   "ds18b20"
-#define SENSOR_TYPE_bmp280    "bmp280"
-#define SENSOR_TYPE_bme680    "bme680"
+#define SENSOR_TYPE_si7021 "si7021"
+#define SENSOR_TYPE_ds18b20 "ds18b20"
+#define SENSOR_TYPE_bmp280 "bmp280"
+#define SENSOR_TYPE_bme680 "bme680"
 
 // Sensorpins
-#define SENSOR_PIN_SDA        0
-#define SENSOR_PIN_SDC        2
-#define SENSOR_PIN_OW         3
+#define SENSOR_PIN_SDA 0
+#define SENSOR_PIN_SDC 2
+#define SENSOR_PIN_OW 3
 
 // Deepsleep
-#define DS_L                  5 //long
-#define DS_S                  2 //short
+#define DS_L 5 // long
+#define DS_S 2 // short
 
 // CC1101
-#define CC_FREQ               868.32
-#define CC_POWER              10
+#define CC_FREQ 868.32
+#define CC_POWER 10
+#define GD0 2
 
-// Verbose
-#define INFO 
-//#define DEBUG
+// OUTPUT
+#define VERBOSE
+// #define DEBUG
--- src/main.cpp
+++ src/main.cpp
@@ -4,49 +4,67 @@
 #include <RadioLib.h>
 #include <VoltageReference.h>
 
-#if __has_include("config.h")
-  #include <config.h>
-#else
-  #error "No Defaults: Copy config.sample.h to config.h and edit that to use your own settings"
-#endif
+// Disable/enable sensors
+// #define SENSOR_TYPE_si7021 "si7021"
+#define SENSOR_TYPE_ds18b20 "ds18b20"
+// #define SENSOR_TYPE_bmp280 "bmp280"
+#define SENSOR_TYPE_bme680 "bme680"
+
+// OUTPUT
+// #define VERBOSE
+// #define DEBUG
+
+// Deepsleep
+#define DS_L 4 // long
+#define DS_S 1 // short
+
+// Sensorpins
+#define SENSOR_PIN_SDA 0
+#define SENSOR_PIN_SDC 2
+#define SENSOR_PIN_OW 3
+
+// CC1101
+#define CC_FREQ 868.32
+#define CC_POWER 10
+#define GD0 2
 
 #ifdef SENSOR_TYPE_si7021
-  #include <Adafruit_Si7021.h>
+#include <Adafruit_Si7021.h>
 #endif
 #ifdef SENSOR_TYPE_ds18b20
-  #include <OneWire.h>
-  #include <DallasTemperature.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 #endif
 #if defined(SENSOR_TYPE_bmp280) || defined(SENSOR_TYPE_bme680)
-  #include <Wire.h>
+#include <Wire.h>
 #endif
 #ifdef SENSOR_TYPE_bmp280
-  #include <Adafruit_BMP280.h>
+#include <Adafruit_BMP280.h>
 #endif
 #ifdef SENSOR_TYPE_bme680
-  #include <Adafruit_BME680.h>
+#include <Adafruit_BME680.h>
 #endif
 
 // CC1101
 // CS pin:    10
 // GDO0 pin:  2
-CC1101 cc = new Module(10, 2, RADIOLIB_NC);
+CC1101 cc = new Module(10, GD0, RADIOLIB_NC);
 
 // voltage
 VoltageReference vRef;
 
 #ifdef SENSOR_TYPE_si7021
-  Adafruit_Si7021 si = Adafruit_Si7021();
+Adafruit_Si7021 si = Adafruit_Si7021();
 #endif
 #ifdef SENSOR_TYPE_ds18b20
-  OneWire oneWire(SENSOR_PIN_OW);
-  DallasTemperature ds18b20(&oneWire);
+OneWire oneWire(SENSOR_PIN_OW);
+DallasTemperature ds18b20(&oneWire);
 #endif
 #ifdef SENSOR_TYPE_bmp280
-  Adafruit_BMP280 bmp280;
+Adafruit_BMP280 bmp280;
 #endif
 #ifdef SENSOR_TYPE_bme680
-  Adafruit_BME680 bme680 = Adafruit_BME680();
+Adafruit_BME680 bme680 = Adafruit_BME680();
 #endif
 
 // counter
@@ -58,10 +76,11 @@ int getUniqueID();
 void sleepDeep(uint8_t t);
 void printHex(uint8_t num);
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   delay(10);
-#ifdef INFO
+#ifdef VERBOSE
   delay(20);
 #endif
   // Start Boot
@@ -70,14 +89,23 @@ void setup() {
   Serial.print(F("> Booting... Compiled: "));
   Serial.println(F(__TIMESTAMP__));
 
-  // Start CC1101
+// Start CC1101
+#ifdef VERBOSE
   Serial.print(F("[CC1101] Initializing... "));
+#endif
   int state = cc.begin(CC_FREQ, 48.0, 48.0, 135.0, CC_POWER, 16);
-  if (state == ERR_NONE) {
+  if (state == ERR_NONE)
+  {
+#ifdef VERBOSE
     Serial.println(F("OK"));
-  } else {
+#endif
+  }
+  else
+  {
+#ifdef VERBOSE
     Serial.print(F("ERR "));
     Serial.println(state);
+#endif
     sleepDeep(DS_S);
   }
 
@@ -85,37 +113,46 @@ void setup() {
   vRef.begin();
 
 #ifdef SENSOR_TYPE_si7021
-  if (!si.begin()){
+  if (!si.begin())
+  {
+#ifdef VERBOSE
     Serial.print(SENSOR_TYPE_si7021);
     Serial.print(": ");
     Serial.println(" ERROR -1");
+#endif
     sleepDeep(DS_S);
   }
 #endif
-	
+
 #ifdef SENSOR_TYPE_ds18b20
   ds18b20.begin();
 #endif
-	
+
 #ifdef SENSOR_TYPE_bmp280
-  bmp280.begin(0x76,0x60); // fix GY-B11 module
+  bmp280.begin(0x76, 0x60); // fix GY-B11 module
 #endif
 
 #ifdef SENSOR_TYPE_bme680
-  if (!bme680.begin()){
+  if (!bme680.begin())
+  {
+#ifdef VERBOSE
     Serial.print(SENSOR_TYPE_bme680);
     Serial.print(": ");
     Serial.println(" ERROR -1");
+#endif
     sleepDeep(DS_S);
   }
 #endif
 }
 
-void loop() {
-#ifdef SENSOR_TYPE_si7021	
-  float si_temperature = si.readTemperature(); 
+void loop()
+{
+#ifdef SENSOR_TYPE_si7021
+  float si_temperature = si.readTemperature();
   float si_humidity = si.readHumidity();
-  if (!isnan(si_temperature)) {
+#ifdef VERBOSE
+  if (!isnan(si_temperature))
+  {
     Serial.print(SENSOR_TYPE_si7021);
     Serial.print(": ");
     Serial.print(si_temperature);
@@ -124,25 +161,31 @@ void loop() {
     Serial.println("%, ");
   }
 #endif
+#endif
 #ifdef SENSOR_TYPE_ds18b20
   ds18b20.requestTemperatures();
   float ds_temperature = ds18b20.getTempCByIndex(0);
-  if (ds_temperature != DEVICE_DISCONNECTED_C) {
-    Serial.print(SENSOR_TYPE_ds18b20);
-    Serial.print(": ");
+#ifdef VERBOSE
+  Serial.print(SENSOR_TYPE_ds18b20);
+  Serial.print(": ");
+  if (ds_temperature != DEVICE_DISCONNECTED_C)
+  {
     Serial.print(ds_temperature);
     Serial.println("C");
-  } else {
-    Serial.print(SENSOR_TYPE_ds18b20);
-    Serial.print(": ");
+  }
+  else
+  {
     Serial.println("ERR");
   }
+#endif
 #endif
 #ifdef SENSOR_TYPE_bmp280
   float bmp280_temperature = bmp280.readTemperature();
   float bmp280_pressure = bmp280.readPressure();
   float bmp280_altitude = bmp280.readAltitude(1013.25);
-  if (!isnan(bmp280_pressure) || bmp280_pressure > 0) {
+#ifdef VERBOSE
+  if (!isnan(bmp280_pressure) || bmp280_pressure > 0)
+  {
     Serial.print(SENSOR_TYPE_bmp280);
     Serial.print(": ");
     Serial.print(bmp280_temperature);
@@ -153,17 +196,21 @@ void loop() {
     Serial.println("m");
   }
 #endif
+#endif
 #ifdef SENSOR_TYPE_bme680
-  if (! bme680.performReading()) {
+  if (!bme680.performReading())
+  {
     Serial.println("[BME680]: ERROR read!");
     sleepDeep(1);
   }
-  float bme680_temperature = bme680.temperature; 
+  float bme680_temperature = bme680.temperature;
   float bme680_humidity = bme680.humidity;
-  float bme680_pressure = bme680.pressure/100.0;
+  float bme680_pressure = bme680.pressure / 100.0;
   float bme680_altitude = bme680.readAltitude(1013.25);
   float bme680_gas = bme680.gas_resistance / 1000.0;
-  if (!isnan(bme680_temperature)) {
+#ifdef VERBOSE
+  if (!isnan(bme680_temperature))
+  {
     Serial.print(SENSOR_TYPE_bme680);
     Serial.print(": ");
     Serial.print(bme680_temperature);
@@ -178,160 +225,221 @@ void loop() {
     Serial.println("KOhms");
   }
 #endif
-  float vcc = vRef.readVcc()/100;
+#endif
+  float vcc = vRef.readVcc() / 100;
+#ifdef VERBOSE
   Serial.print("VCC: ");
   Serial.print(vcc);
+#endif
 
   // prepare msg string
   String str = "M";
-#ifdef COUNTER
+#ifdef DEBUG
   str += ",I:";
   str += msgCounter;
 #endif
   str += ",N:";
   str += String(getUniqueID(), HEX);
-#ifdef SENSOR_TYPE_si7021	
-  if (!isnan(si_temperature)) {
+#ifdef SENSOR_TYPE_si7021
+  if (!isnan(si_temperature))
+  {
     str += ",T1:";
-    str += int(round(si_temperature*10));
+    str += int(round(si_temperature * 10));
     str += ",H1:";
-    str += int(round(si_humidity*10));
+    str += int(round(si_humidity * 10));
   }
 #endif
 #ifdef SENSOR_TYPE_ds18b20
-  if (ds_temperature != DEVICE_DISCONNECTED_C) {
+  if (ds_temperature != DEVICE_DISCONNECTED_C)
+  {
     str += ",T2:";
-    str += int(round(ds_temperature*10));
+    str += int(round(ds_temperature * 10));
   }
 #endif
 #ifdef SENSOR_TYPE_bmp280
-  if (!isnan(bmp280_pressure) && bmp280_pressure > 0) {
+  if (!isnan(bmp280_pressure) && bmp280_pressure > 0)
+  {
     str += ",T3:";
-    str += int(round(bmp280_temperature*10));
+    str += int(round(bmp280_temperature * 10));
     str += ",P3:";
-    str += int(round(bmp280_pressure)/10);
+    str += int(round(bmp280_pressure) / 10);
     str += ",A3:";
     str += int(round(bmp280_altitude));
   }
 #endif
 #ifdef SENSOR_TYPE_bme680
-  if (!isnan(bme680_temperature)) {
+  if (!isnan(bme680_temperature))
+  {
     str += ",T4:";
-    str += int(round(bme680_temperature*10));
+    str += int(round(bme680_temperature * 10));
     str += ",H4:";
-    str += int(round(bme680_humidity*10));
+    str += int(round(bme680_humidity * 10));
     str += ",P4:";
-    str += int(round(bme680_pressure*10));
+    str += int(round(bme680_pressure * 10));
     str += ",A4:";
     str += int(round(bme680_altitude));
     str += ",Q4:";
     str += int(round(bme680_gas));
   }
-#endif	
+#endif
   str += ",V1:";
   str += int(vcc);
 
-  if (str.length() > 60){
+  if (str.length() > 60)
+  {
+#ifdef VERBOSE
     Serial.print(F("> String too long: "));
     Serial.println(str.length());
+#endif
     sleepDeep(DS_S);
-  } else {
+  }
+  else
+  {
+#ifdef VERBOSE
     Serial.print(F("> String length: "));
     Serial.println(str.length());
-    //str += ",E:";
+#endif
+    // str += ",E:";
     int str_diff = 60 - str.length();
-    if (str_diff >= 1){ str += ",";}
-    if (str_diff >= 2){ str += "E";}
-    if (str_diff >= 3){ str += ":";}
+    if (str_diff >= 1)
+    {
+      str += ",";
+    }
+    if (str_diff >= 2)
+    {
+      str += "E";
+    }
+    if (str_diff >= 3)
+    {
+      str += ":";
+    }
 
     // max length is 62 because of Arduino String last byte 00
     // but 62 not good better use 61
     // String length here to 60, thus packet length 61
-    for (uint8_t i = str.length(); i < 60; i++){
+    for (uint8_t i = str.length(); i < 60; i++)
+    {
       str += "0";
     }
   }
+#ifdef VERBOSE
   Serial.print(F("[CC1101] Transmitting packet... "));
-  // String to byte +1 string nul terminator 00 and overwrite 
-  byte byteArr[str.length()+1];
-  str.getBytes(byteArr,sizeof(byteArr));
-  byteArr[sizeof(byteArr)/sizeof(byteArr[0])-1] = '0';
-  //Serial.print("Packet Length: ");
-  //Serial.println(sizeof(byteArr)/sizeof(byteArr[0])); // +1 
-  int state = cc.transmit(byteArr,sizeof(byteArr)/sizeof(byteArr[0]));
+#endif
+  // String to byte +1 string nul terminator 00 and overwrite
+  byte byteArr[str.length() + 1];
+  str.getBytes(byteArr, sizeof(byteArr));
+  byteArr[sizeof(byteArr) / sizeof(byteArr[0]) - 1] = '0';
+  // Serial.print("Packet Length: ");
+  // Serial.println(sizeof(byteArr)/sizeof(byteArr[0])); // +1
+  int state = cc.transmit(byteArr, sizeof(byteArr) / sizeof(byteArr[0]));
 
-  if (state == ERR_NONE) {
+  if (state == ERR_NONE)
+  {
+#ifdef VERBOSE
     Serial.println(F("OK"));
-	  Serial.println(str);
-    for(uint8_t i=0; i<sizeof(byteArr); i++){
+#endif
+    Serial.println(str);
+#ifdef DEBUG
+    for (uint8_t i = 0; i < sizeof(byteArr); i++)
+    {
       printHex(byteArr[i]);
     }
     Serial.println("");
-  } else if (state == ERR_PACKET_TOO_LONG) {
+#endif
+  }
+#ifdef VERBOSE
+  else if (state == ERR_PACKET_TOO_LONG)
+  {
     // the supplied packet was longer than 64 bytes
     Serial.println(F("ERR: too long!"));
-  } else {
+  }
+  else
+  {
     // some other error occurred
     Serial.print(F("ERR, code "));
     Serial.println(state);
   }
+#endif
   sleepDeep(DS_L);
-#ifdef COUNTER
+#ifdef DEBUG
   msgCounter++;
 #endif
 }
 
 // Last 4 digits of ChipID
-int getUniqueID(){
+int getUniqueID()
+{
   int uid = 0;
   // read EEPROM serial number
   int address = 13;
   int serialNumber;
-	Serial.println();
-  if (EEPROM.read(address) != 255){
+#ifdef VERBOSE
+  Serial.println();
+#endif
+  if (EEPROM.read(address) != 255)
+  {
     EEPROM.get(address, serialNumber);
     uid = serialNumber;
-	  Serial.print("EEPROM SN: ");
-	  Serial.print(uid);
+#ifdef VERBOSE
+    Serial.print("EEPROM SN: ");
+    Serial.print(uid);
     Serial.print(" -> HEX: ");
     Serial.println(String(serialNumber, HEX));
-  } else {
-    Serial.println("EEPROM SN: ERROR EMPTY!");
+#endif
   }
-    return uid;
+#ifdef VERBOSE
+  else
+  {
+    Serial.println("EEPROM SN: ERROR EMPTY USING DEFAULT");
+  }
+#endif
+  return uid;
 }
 
 // sleep
 // 1 - 254 minutes
 // 255 = 8 seconds
 // 0 = forever
-void sleepDeep(uint8_t t) {
+void sleepDeep(uint8_t t)
+{
   uint8_t m = 60;
-#ifdef INFO
+#ifdef VERBOSE
   Serial.print("Deep Sleep: ");
-  if (t < 1){
+  if (t < 1)
+  {
     Serial.println("forever");
-  } else if (t > 254){
+  }
+  else if (t > 254)
+  {
     t = 1;
     m = 8;
     Serial.println("8s");
-  } else {
+  }
+  else
+  {
     Serial.print(t);
     Serial.println("min");
   }
 #endif
   delay(500); // 70 100 500 1000
-  if (t > 0){
-    for (int8_t i = 0; i < (t * m / 8); i++) {
+  if (t > 0)
+  {
+    for (int8_t i = 0; i < (t * m / 8); i++)
+    {
       LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
     }
-  } else {
-      LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+  }
+  else
+  {
+    LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
   }
 }
 
-void printHex(uint8_t num) {
+#ifdef DEBUG
+void printHex(uint8_t num)
+{
   char hexCar[2];
   sprintf(hexCar, "%02X", num);
   Serial.print(hexCar);
 }
+#endif
