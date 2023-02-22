0ebb9403013f7d9e69bbfc54276debdd3db4d13e
13 <eggerben@gmail.com>
2023-02-22 20:09:25 +0100

    fixed byte array

--- src/credentials_example.h
+++ src/credentials_example.h
@@ -9,14 +9,14 @@
 // OUTPUT
 #define VERBOSE
 // #define DEBUG
-// #define GD0 2 // Disable for new method without GD0
+#define GD0 2 // Disable for new method without GD0
 #define CC_FREQ 868.32
-#define CC_POWER 12
+#define CC_POWER 10
 #define CC_DELAY 100 // [100]
 
 // Type
-#define SEND_CHAR
-// #define SEND_BYTE
+// #define SEND_CHAR
+#define SEND_BYTE
 
 // Deep sleep
 #define DS_L 255 // long [3]
@@ -36,4 +36,7 @@
 // GIT
 #ifndef GIT_VERSION
 #define GIT_VERSION "unknown"
+#endif
+#ifndef GIT_VERSION_SHORT
+#define GIT_VERSION_SHORT "0"
 #endif
